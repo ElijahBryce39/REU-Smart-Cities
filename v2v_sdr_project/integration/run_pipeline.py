cceb3811c0818306cbabc5da4745daebaf2ce01f
pollyjuice74 <hernandez.aht82836@gmail.com>
2025-06-10 17:51:24 -0400

    add V2VTxRx and cloned gr-ieee802_11

--- v2v_sdr_project/integration/run_pipeline.py
+++ v2v_sdr_project/integration/run_pipeline.py
@@ -2,7 +2,7 @@ from integration.message_builder.builder import build_cam_message
 from integration.message_router.router import route_to_file
 
 def main():
-    cam_msg = build_cam_message(mock=True)
+    cam_msg = build_cam_message(mock=False)
     route_to_file(cam_msg)
 
 if __name__ == "__main__":
